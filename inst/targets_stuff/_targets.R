6a3c36f5145c7fb195ee5b94009d5dd008b58ce2
vjcitn <stvjc@channing.harvard.edu>
2025-12-06 05:01:24 -0500

    Remove large file from commit

--- inst/targets_stuff/_targets.R
+++ inst/targets_stuff/_targets.R
@@ -9,34 +9,48 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("AnnotationWorkflow")
+  packages = c("AnnotationWorkflow", "RSQLite")
 )
 
-# Run the R scripts in the R/ folder with your custom functions:
-#tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
-
-# Replace the target list below with your own:
 list(
   tar_target(
     name = biocGOinst,  # pulls OBO for GO from geneontology.org
     command = AnnotationWorkflow::getGOobo(destdir=".")
   ),
   tar_target(
-    name = parsed_obo,  # long-running, transforms OBO to text files in folder abc
-    command = AnnotationWorkflow::parseOBO(biocGOinst, "abc"),
-    format = "file"
-  ),
-  tar_target(
-    name = textSet,   # gets and names paths to text files
+    name = textSet,   # gets and names paths to text files after "parsing"
     command = {
-      fs = dir("abc", full=TRUE)
+      AnnotationWorkflow::parseOBO(biocGOinst, "newOBOtxt")
+      fs = dir("newOBOtxt", full=TRUE)
       names(fs) = gsub(".txt", "", basename(fs))
       fs
       }
   ),
   tar_target(
-    name = bar,   # tests the naming
-    command = print(textSet["graph_path"])
-  )
+    name = emptyGOsql,
+    command = {
+      source("make_basicGO.R")
+      "basicGO.sqlite"
+     }
+   ),
+  tar_target(
+    name = populateGOsql,
+    command = {
+      con = RSQLite::dbConnect(RSQLite::SQLite(), emptyGOsql)
+      graph_df = read.delim(textSet["graph_path"], sep="\t")
+      RSQLite::dbWriteTable(con, "graph_path", graph_df, overwrite=TRUE)
+      term_df = read.delim(textSet["term"], sep="\t")
+      RSQLite::dbWriteTable(con, "term", term_df, overwrite=TRUE)
+      term2term_df = read.delim(textSet["term2term"], sep="\t")
+      RSQLite::dbWriteTable(con, "term2term", term2term_df, overwrite=TRUE)
+      term_definition_df = read.delim(textSet["term_definition"], sep="\t")
+      RSQLite::dbWriteTable(con, "term_definition", term_definition_df, overwrite=TRUE)
+      term_synonym_df = read.delim(textSet["term_synonym"], sep="\t")
+      RSQLite::dbWriteTable(con, "term_synonym", term_synonym_df, overwrite=TRUE)
+      m = read.csv("meta.csv")  # FIXME - don't do by hand
+      RSQLite::dbWriteTable(con, "metadata", m[,-1], overwrite=TRUE)
+      RSQLite::dbDisconnect(con)
+      "basicGO.sqlite"
+    }
+   )
 )
